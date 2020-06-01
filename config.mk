a7cd0c754bf8b8f57f32ebb2ca180c04b7c645c2
Anton Yabchinskiy <arn@bestmx.net>
2020-06-02 00:10:54 +0300

    Apply 'Remove-custom-library-search-paths-from-Makefiles' from Debian

--- config.mk
+++ config.mk
@@ -11,8 +11,8 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr
+INCS =
+LIBS = -lc -lcrypt -lX11 -lXext -lXrandr
 
 # flags
 CPPFLAGS += -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H
